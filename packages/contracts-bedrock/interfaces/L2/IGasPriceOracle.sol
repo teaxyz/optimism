d60106407bc6eaedac6002a234dad0af8ef42990
Nikita Meshcheriakov <root@nikitam.io>
2025-12-10 13:21:37 -0300

    interface fix

--- packages/contracts-bedrock/interfaces/L2/IGasPriceOracle.sol
+++ packages/contracts-bedrock/interfaces/L2/IGasPriceOracle.sol
@@ -28,7 +28,7 @@ interface IGasPriceOracle {
 
     function updateGasTokenPriceRatio() external;
     function convertETHToTea(uint256) external view returns (uint256);
-    function teaPerETH() external view returns (uint160);
+    function teaPerETH() external view returns (bool,uint160);
     function getLatestPrice() external view returns (uint96, uint160);
     function getOracleConfig() external view returns (address,uint16,uint80,bool,address);
     function setOracleConfig(uint16,uint80,address) external;
